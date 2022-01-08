d4d9ba7cb385ed2e1667257de6c61cb458a5a737
Carlos Alencar <iuryalencar@poli.ufrj.br>
2022-01-07 22:14:41 -0300

    add topbar images

--- src/Game.ts
+++ src/Game.ts
@@ -2,11 +2,17 @@ export class Game {
     context: CanvasRenderingContext2D;
     gameScreenWidth: number;
     gameScreenHeight: number;
+    topbarHeight: number;
+    floorHeight: number;
+    lifeSize: number;
 
     constructor (context: CanvasRenderingContext2D, gameScreenWidth: number, gameScreenHeight: number) {
         this.context = context;
         this.gameScreenHeight = gameScreenHeight;
         this.gameScreenWidth = gameScreenWidth;
+        this.topbarHeight = 100;
+        this.floorHeight = 50;
+        this.lifeSize = 40;
     }
 
     drawBackground() {
@@ -14,12 +20,40 @@ export class Game {
         this.context.fillRect(0, 0, this.gameScreenWidth, this.gameScreenHeight);
         
         this.context.fillStyle = 'black';
-        this.context.fillRect(0, 0, this.gameScreenWidth, 100);
+        this.context.fillRect(0, 0, this.gameScreenWidth, this.topbarHeight);
         
-        this.context.moveTo(0, this.gameScreenHeight - 50);
-        this.context.lineTo(this.gameScreenWidth, this.gameScreenHeight - 50);
+        this.context.moveTo(0, this.gameScreenHeight - this.floorHeight);
+        this.context.lineTo(this.gameScreenWidth, this.gameScreenHeight - this.floorHeight);
         this.context.strokeStyle = 'grey';
         this.context.stroke();
+    }
+
+    drawLife (hasLife: boolean, xPos: number) {
+        const life = new Image(60,60);
+        life.src = hasLife ? './assets/sprites/heart.png' : './assets/sprites/heart-empty.png';
+
+        const centralizedLifeHeight = this.topbarHeight / 2 - this.lifeSize / 2;
+
+        life.onload = () => this.context.drawImage(life, xPos, centralizedLifeHeight, this.lifeSize, this.lifeSize);
+    }
+
+    drawLifes () {
+        const lifes = [true, true, true, false]
+        let xPos = 20;
+
+        lifes.map(hasLife => {
+            this.drawLife(hasLife, xPos);
+            xPos += this.lifeSize + 20;
+        })
+    }
+
+    drawScore () {
+        const score = 157;
+        const centralizedHeightTopbarContent = this.topbarHeight / 2 + 5;
+        this.context.fillStyle = "white";
+        this.context.font = "25px Arial";
+        this.context.fillText(`Score: ${score}`, 0.65 * this.gameScreenWidth, centralizedHeightTopbarContent);
+        
     }
 
     drawPlayer () {
@@ -30,6 +64,8 @@ export class Game {
     
     start () {
         this.drawBackground();
+        this.drawLifes();
+        this.drawScore();
         this.drawPlayer();
     }
 }
