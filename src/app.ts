c4b8ba266a71beabd1caedc441e19e31bd989446
Carlos Alencar <iuryalencar@poli.ufrj.br>
2022-01-09 16:20:56 -0300

    add touchscreen moves

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,6 @@ canvas.height = gameScreenHeight;
 
 const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
-
-
 const game = new Game (context, gameScreenWidth, gameScreenHeight);
 
 addEventListener('keyup', (event) => {
@@ -29,6 +27,30 @@ addEventListener('keyup', (event) => {
         game.turnOffPlayerMovement();
     }
 });
+
+let lastClientX = gameScreenWidth / 2;
+addEventListener('touchmove', (event) => {
+    const direction = event.touches[0].clientX - lastClientX;
+
+    if (direction < -5) {
+        //esquerda
+        game.turnOffPlayerMovement();
+        game.turnOnPlayerMovement('left');
+    }
+
+    if (direction > 5) {
+        // direita
+        game.turnOffPlayerMovement();
+        game.turnOnPlayerMovement('right');
+    }
+
+    lastClientX = event.touches[0].clientX
+});
+
+addEventListener('touchend', () => {
+    game.turnOffPlayerMovement();
+})
+
 game.start();
 
 
